4a401b21edb445a181fbf8d9222f8c7a3532afa5
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-03-19 16:53:11 +0100

    js question 1 save

--- timer.js
+++ timer.js
@@ -1,3 +1,98 @@
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+];
 const FULL_DASH_ARRAY = 283;
 
 const COLOR_CODES = {
@@ -29,6 +124,7 @@ document.getElementById("app").innerHTML = `
         "
       ></path>
     </g>
+    <span class = 'reman'>REMAINING</span>
     <span class="text">SECONDS</span>
   </svg>
   <span id="base-timer-label" class="base-timer__label"> ${formatTime(
@@ -87,3 +183,39 @@ function setCircleDasharray() {
     .getElementById("base-timer-path-remaining")
     .setAttribute("stroke-dasharray", circleDasharray);
 }
+let questionNum = 0;
+const buttonsDiv = document.querySelector(".colum");
+
+for (let i = 0; i < questions[questionNum].length; i++) {
+  for (let j = 0; j < questions[questionNum].incorrect_answers.length; j++) {
+    const buttons = document.createElement("button");
+    buttons.classList.add("button");
+    buttonsDiv.appendChild(buttons);
+    questionNum++;
+  }
+}
+console.log(questions[0].incorrect_answers.length);
+/*const button = document.querySelectorAll(".button");
+const questionsH = document.querySelector(".transpTitle");
+let questionNum = 0;
+const quiz = () => {
+  questionsH.innerText = questions[questionNum].question;
+};
+const options = () => {};
+for (let i = 1; i < button.length; i++) {
+  for (let j = 0; j < questions[questionNum].incorrect_answers.length; j++) {
+    button[i].innerText = questions[questionNum].incorrect_answers[j];
+
+    button.innerText = questions[questionNum].correct_answer;
+    questionNum++;
+  }
+}
+for (i = 0; i < button.length; i++) {
+  const butt = button[i];
+  butt.onclick = function () {
+    butt.classList.add("butt");
+    questionNum++;
+    quiz();
+    options();
+  };
+*/
