f1f6582bdf34145f080afbb947d6c8055bd03ec7
chiarafais <chiara0915@gmail.com>
2024-03-21 21:32:18 +0100

    extra domande

--- welcome.js
+++ welcome.js
@@ -1,10 +1,15 @@
 // WELCOME ZONE
 const buttonProceed = function () {
   const checkbox = document.querySelector(".css-checkbox");
+  const difficult = document.querySelector("#difficult");
+  const quantity = document.querySelector("#quantity");
+
   if (checkbox.checked) {
+    sessionStorage.setItem("difficult", difficult.value);
+    sessionStorage.setItem("quantity", quantity.value);
     window.location.href = "Benchmark.html";
   } else {
     const obbligatorio = document.getElementById("campoObb");
-    obbligatorio.innerText = "* campo obbligatorio per procedere";
+    obbligatorio.innerText = "* Required field to continue";
   }
 };
